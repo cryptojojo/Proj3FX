4ab55c104e67a4920c46cb6863918beea17ee99e
Crypto Jojo <cryptojojo1@gmail.com>
2018-04-04 20:27:42 -0500

    First commit, has a layout for javafx and has scotts elevator code

---
+++ src/application/Main.java
@@ -0,0 +1,25 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		// just load fxml file and display it in the stage:
+
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("application.fxml"));
+		Parent root = loader.load();
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/elevator/ElevatorDriver.java
@@ -0,0 +1,196 @@
+package elevator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Random;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Elevator Simulation
+ * @author kerlin
+ */
+public class ElevatorDriver
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        //Setup Building
+        PriorityQueue<ElevatorRider>[] building = new PriorityQueue[5];
+        for (int i = 0; i < building.length; i++)
+            building[i] = new PriorityQueue();
+        
+        //Setup riders
+        ArrayList<ElevatorRider> riders = new ArrayList();
+        addRiders(110,5,riders);
+        addRiders(75,4,riders);
+        addRiders(65,3,riders);
+        addRiders(100,2,riders);
+        Collections.shuffle(riders);
+
+        //Make VIPs
+        for(int i = 0; i < riders.size() / 10; i++)
+        {
+            riders.get(i).promote();
+        }
+        
+        Collections.shuffle(riders);
+        
+        //Setup Elevator
+        Elevator elevator = new Elevator();
+        
+        //Test code to make sure that the riders worked!
+        /*
+        for (ElevatorRider rider: riders)
+            System.out.println(rider);
+        */
+        
+        //Morning Mode
+        int currentRider = 0;
+        do
+        {
+            //New rider!
+            if (currentRider < riders.size())
+            {
+                //10 new riders
+                for (int index = 0; index < 10; index++)
+                {
+                    building[0].add(riders.get(currentRider++));
+                }
+            }
+            
+            //Pop riders for elevator's current floor
+            while(elevator.peek() != null && elevator.peek().getHomeFloor() == elevator.getCurrentFloor())
+            {
+                building[elevator.getCurrentFloor() - 1].add(elevator.pop());
+            }
+            
+            //System.out.println("AFTER POPS" + elevator);
+            
+            //Frustrate
+            elevator.frustrate();
+            
+            
+            //Push riders from current floor to elevator
+            while(elevator.getCurrentFloor() == 1 && !elevator.isFull() && !building[elevator.getCurrentFloor() - 1].isEmpty())
+                elevator.push(building[elevator.getCurrentFloor() - 1].remove());
+            
+            //System.out.println("ELEVATOR BEFORE MOVE: " + elevator);
+            
+            //Move elevator
+            if (elevator.peek() != null)
+                elevator.setCurrentFloor(elevator.peek().getHomeFloor());
+            else
+                elevator.setCurrentFloor(1);
+            
+            System.out.println("ELEVATOR AT END: " + elevator);
+        }
+        while(currentRider < riders.size() || elevator.peek() != null || building[0].peek() != null);
+        
+        //Output
+        int result = 0;
+        int resultVIP = 0;
+        int resultNoVIP = 0;
+        for (ElevatorRider rider : riders)
+        {
+            result += rider.getFrustration();
+            if (rider.isVIP())
+                resultVIP += rider.getFrustration();
+            else
+                resultNoVIP += rider.getFrustration();
+        }
+        
+        System.out.println("AM MODE:\n\tAverage (MEAN) Frustration Level is: " + ((double) result / riders.size()));
+        System.out.println("AM MODE:\n\tAverage (MEAN) VIP Frustration Level is: " + ((double) resultVIP / (riders.size()/10)));
+        System.out.println("AM MODE:\n\tAverage (MEAN) Non-VIP Frustration Level is: " + ((double) resultNoVIP / (riders.size()*.9)));
+
+        
+        
+        //Evening Mode
+        //Reset Starting Conditions
+        currentRider = 0;
+        for (int i = 0; i < riders.size(); i++)
+            riders.get(i).setFrustration(0);
+        for (int i = 0; i < building.length; i++)
+            while (!building[i].isEmpty())
+                building[i].remove();
+        do
+        {
+            //New rider!
+            if (currentRider < riders.size())
+            {
+                //10 new riders
+                for (int index = 0; index < 10; index++)
+                {
+                    building[riders.get(currentRider).getHomeFloor() - 1].add(riders.get(currentRider));
+                    riders.get(currentRider).setHomeFloor(1);
+                    currentRider++;
+                }
+            }
+            
+            //Pop riders for elevator's current floor
+            while(elevator.peek() != null && elevator.peek().getHomeFloor() == elevator.getCurrentFloor())
+            {
+                building[elevator.getCurrentFloor() - 1].add(elevator.pop());
+            }
+            
+            //System.out.println("AFTER POPS" + elevator);
+            
+            //Frustrate
+            elevator.frustrate();
+            
+            
+            //Push riders from current floor to elevator
+            while(elevator.getCurrentFloor() != 1 && !elevator.isFull() && !building[elevator.getCurrentFloor() - 1].isEmpty())
+                elevator.push(building[elevator.getCurrentFloor() - 1].remove());
+            
+            //System.out.println("ELEVATOR BEFORE MOVE: " + elevator);
+            
+           
+            //Move elevator
+            Random gen = new Random();
+            if (elevator.peek() != null)
+                elevator.setCurrentFloor(elevator.peek().getHomeFloor());
+            else
+                elevator.setCurrentFloor(gen.nextInt(4) + 2);
+            
+            System.out.println("ELEVATOR AT END: " + elevator);
+            //System.out.println(building[1]);
+        }
+        while(currentRider < riders.size() || elevator.peek() != null || building[1].peek() != null || building[2].peek() != null || building[3].peek() != null || building[4].peek() != null);
+        
+        //Output
+        result = 0;
+        resultVIP = 0;
+        resultNoVIP = 0;
+        for (ElevatorRider rider : riders)
+        {
+            result += rider.getFrustration();
+            if (rider.isVIP())
+                resultVIP += rider.getFrustration();
+            else
+                resultNoVIP += rider.getFrustration();
+        }
+        
+        System.out.println("PM MODE:\n\tAverage (MEAN) Total Frustration Level is: " + ((double) result / riders.size()));
+        System.out.println("PM MODE:\n\tAverage (MEAN) VIP Frustration Level is: " + ((double) resultVIP / (riders.size()/10)));
+        System.out.println("PM MODE:\n\tAverage (MEAN) Non-VIP Frustration Level is: " + ((double) resultNoVIP / (riders.size()*.9)));
+        
+    }
+    
+    public static void addRiders(int quantity, int floor, ArrayList<ElevatorRider> list)
+    {
+        for (int i = 0; i < quantity; i++)
+            list.add(new ElevatorRider(floor));
+    }
+  
+}
